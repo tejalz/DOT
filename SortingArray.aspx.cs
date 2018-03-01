f553cee47a9213cd9f1764b2f757d4a7784cccbb
GovindarajanMa <31079345+GovindarajanMa@users.noreply.github.com>
2018-03-01 16:58:47 -0500

    Add files via upload

---
+++ SortingArray.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SortingArray : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        int[] i = new int[5] { 2,4,6,10,8};
+        Array.Sort(i);
+        foreach(int j in i) { Response.Write(j); }
+    }
+}
