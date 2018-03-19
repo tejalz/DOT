df734f969f7b06b3cd81e9e30571a086cf43b2be
GovindarajanMa <31079345+GovindarajanMa@users.noreply.github.com>
2018-03-19 11:09:47 -0400

    Add files via upload

---
+++ MassDOTMap.js
@@ -0,0 +1,249 @@
+﻿//Define the namespaces we'll be using
+window.mdot = window.mdot || {};
+window.mdot.maps = window.mdot.maps || {};
+window.mdot.maps.plugins = window.mdot.maps.plugins || {};
+
+function toggleFullScreen(map) {
+    var center;
+    var $mapCanvas = $(map.getDiv());
+    var $mapWrapper = $mapCanvas.parent();
+
+    if (map.fullscreen == false) {
+        center = map.getCenter();
+
+        $mapCanvas.css("position", "fixed")
+            .css("top", 0)
+            .css("left", 0)
+            .css("width", "100%")
+            .css("height", "100%");
+
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+        map.fullscreen = true;
+    } else {
+        center = map.getCenter();
+        $mapCanvas.css("position", "relative");
+        $mapCanvas.width($mapWrapper.width());
+        $mapCanvas.height(map.defaultHeight);
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+        map.fullscreen = false;
+    }
+}
+
+(function($) {
+    var methods = {
+        init: function() {
+            return this.each(function(index) {
+                var map,
+                    $mapWrapper = $(this),
+                    $mapCanvas = $mapWrapper.find(".map_canvas"),
+                    modID = $mapCanvas.find("input:hidden").val(),
+                    options = mdot["maps"]["map" + modID]["options"],
+                    defaultHeight = options["height"] + "px",
+                    defaultLat = options["latitude"],
+                    defaultLon = options["longitude"],
+                    defaultZoom = options["zoom"],
+                    minZoom = options["minZoom"],
+                    maxZoom = options["maxZoom"],
+                    defaultStyles = options["styles"],
+                    mapTypeId = options["mapTypeId"],
+                    panDistance = 128; // Pan distance is half a tile's width
+
+                //Size the map's div to the height from the module settings
+                $mapCanvas.css("height", defaultHeight);
+
+                // Create the map
+                map = new google.maps.Map(document.getElementById($mapCanvas.attr("id")), {
+                    disableDefaultUI: true,
+                    //CJM 
+                    //disableDoubleClickZoom: true,
+                    //CJM
+                    scrollwheel: false,
+                    mapTypeId: google.maps.MapTypeId[mapTypeId],
+                    center: new google.maps.LatLng(defaultLat, defaultLon),
+                    zoom: defaultZoom,
+                    minZoom: minZoom,
+                    maxZoom: maxZoom,
+                    disableAutoPan: true,
+                    styles: defaultStyles
+                });
+
+                //Add custom properties to the map
+                map.defaultHeight = defaultHeight;
+                map.fullscreen = false;
+                map.defaultZoom = defaultZoom;
+                var infoWindowHeight = 240; //50;
+                var infoWindowWidth = 352; //150;
+                map.infoWindow = new google.maps.InfoWindow({ size: new google.maps.Size(infoWindowWidth, infoWindowHeight) });
+
+                //Create the map's controls collection
+                var $controlsPan = $("<div />", {
+                        'class': "pan"
+                    }),
+                    $controlsUp = $("<button />", {
+                        'name': "pan-up",
+                        'class': "pan-up",
+                        click: function(e) {
+                            e.preventDefault();
+                            map.panBy(0, -panDistance);
+                        }
+                    }),
+                    $controlsLeft = $("<button />", {
+                        'name': "pan-left",
+                        'class': "pan-left",
+                        click: function(e) {
+                            e.preventDefault();
+                            map.panBy(-panDistance, 0);
+                        }
+                    }),
+                    $controlsRight = $("<button />", {
+                        'name': "pan-right",
+                        'class': "pan-right",
+                        click: function(e) {
+                            e.preventDefault();
+                            map.panBy(panDistance, 0);
+                        }
+                    }),
+                    $controlsDown = $("<button />", {
+                        'name': "pan-down",
+                        'class': "pan-down",
+                        click: function(e) {
+                            e.preventDefault();
+                            map.panBy(0, panDistance);
+                        }
+                    }),
+                    $controlsCenter = $("<button />", {
+                        'name': "pan-center",
+                        'class': "pan-center",
+                        click: function(e) {
+                            e.preventDefault();
+                            map.setZoom(defaultZoom);
+                            map.setCenter(new google.maps.LatLng(defaultLat, defaultLon));
+                        }
+                    }),
+                    $controlsSpan = $("<span style=\"display:block\" />"),
+                    $controlsZoom = $("<div />", {
+                        'class': "zoom"
+                    }),
+                    $controlsOut = $("<button />", {
+                        'name': "zoom-out",
+                        'class': "zoom-out",
+                        click: function(e) {
+                            e.preventDefault();
+                            map.setZoom(map.getZoom() - 1);
+                        }
+                    }),
+                    $controlsIn = $("<button />", {
+                        'name': "zoom-in",
+                        'class': "zoom-in",
+                        click: function(e) {
+                            e.preventDefault();
+                            map.setZoom(map.getZoom() + 1);
+                        }
+                    }),
+                    $controlsFullScreen = $("<button />", {
+                        'name': "full-screen",
+                        'class': "map_toggle_fullscreen",
+                        click: function(e) {
+                            e.preventDefault();
+                            toggleFullScreen(map);
+                            if (map.fullscreen == true) {
+                                $(this).find("img").attr("alt", "Exit full screen").attr("src", window.mdot["mapImagesURL"] + "fsExit.png");
+                            } else {
+                                $(this).find("img").attr("alt", "Viewe full screen").attr("src", window.mdot["mapImagesURL"] + "fsEnter.png");
+                            }
+                        }
+                    });
+
+                // Create the controls
+                $mapCanvas.attr("tabIndex", 0);
+                // Pan controls
+                $controlsUp.append("<img src=\"pan-up.png\" alt=\"Pan map up\">");
+                $controlsLeft.append("<img src=\"pan-left.png\" alt=\"Pan map left\">");
+                $controlsCenter.append("<img src=\"center.png\" alt=\"Center map\">");
+                $controlsRight.append("<img src=\"pan-right.png\" alt=\"Pan map right\">");
+                $controlsDown.append("<img src=\"pan-down.png\" alt=\"Pan map down\">");
+                //alert(window.mdot["mapImagesURL"]);
+                $controlsSpan.append($controlsLeft, $controlsCenter, $controlsRight);
+                $controlsPan.append($controlsUp, $controlsSpan, $controlsDown);
+
+                // Zoom controls
+                $controlsIn.append("<img src=\"zoom-in.png\" alt=\"Zoom in on map\">");
+                $controlsOut.append("<img src=\"zoom-out.png\" alt=\"Zoom out of map\">");
+
+                //Full Screen control
+                $controlsFullScreen.append("<img src=\"fsEnter.png\" alt=\"View full screen\">");
+
+                $controlsZoom.append($controlsFullScreen, $controlsIn, $controlsOut);
+
+                // Add to the page
+                map.controls[google.maps.ControlPosition.TOP_LEFT].push($controlsPan[0]);
+                map.controls[google.maps.ControlPosition.TOP_RIGHT].push($controlsZoom[0]);
+
+                $mapCanvas.keydown(function(e) {
+                    // If the keycode isn't in this group, stop right here
+                    if ([37, 38, 39, 40, 109, 189, 219, 107, 187, 221].indexOf(e.keyCode) == -1) {
+                        return;
+                    }
+
+                    e.preventDefault();
+                    e.stopPropagation();
+
+                    switch (e.keyCode) {
+                    case 37: // left arrow
+                        map.panBy(-panDistance, 0);
+                        break;
+                    case 38: // up arrow
+                        map.panBy(0, -panDistance);
+                        break;
+                    case 39: // right arrow
+                        map.panBy(panDistance, 0);
+                        break;
+                    case 40: // down arrow
+                        map.panBy(0, panDistance);
+                        break;
+                    case 109: // numpad -
+                    case 189: // minus
+                    case 219: // open bracket
+                        map.setZoom(map.getZoom() - 1);
+                        break;
+                    case 107: // numpad +
+                    case 187: // equal
+                    case 221: // close bracket
+                        map.setZoom(map.getZoom() + 1);
+                        break;
+                    }
+                });
+
+                mdot["maps"]["map" + modID]["initPlugins"](map);
+                google.maps.event.addDomListener(window, "resize", function() {
+                    var center = map.getCenter();
+                    $mapCanvas = $mapWrapper.find(".map_canvas");
+                    if (map.fullscreen == false)
+                        $mapCanvas.width($mapWrapper.width());
+                    google.maps.event.trigger(map, "resize");
+                    map.setCenter(center);
+                });
+                window.mdot["maps"]["map" + modID] = map;
+            });
+        }
+    };
+
+    $.fn.map = function(method) {
+        var $map = this;
+
+        // Method calling logic
+        if (methods[method]) {
+            return methods[method].apply($map, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === "object" || !method) {
+            return methods.init.apply($map, arguments);
+        } else {
+            $.error("Method " + method + " does not exist on jQuery.map");
+        }
+    };
+})(jQuery);
+
+function buildMaps() {
+    $(".map").map();
+}
---
+++ ers.js
@@ -0,0 +1,181 @@
+﻿/// <reference path="jquery-vsdoc.js" />
+/// <reference path="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" />
+/// <reference path="maps.googleapis.com/maps/api/js?v=3&sensor=false" />
+/// <reference path="google-maps-3-vs-1-0.js" />
+mdot["maps"]["plugins"]["ers"] = mdot["maps"]["plugins"]["ers"] || {};
+mdot["maps"]["plugins"]["ers"]["Incidents"] = mdot["maps"]["plugins"]["ers"]["Incidents"] || function (map, options)
+{
+    "use strict";
+    var
+        that = this, //used in internal methods
+        $plannedCheckbox = $(options["plannedCheckbox"])
+        .change(function ()
+        {
+            if ($(this).prop("checked"))
+            {
+                for (var i = that.currentCount; i < that.length; i++)
+                {
+                    that[i].setMap(map);
+                }
+            } else
+            {
+
+                for (var i = that.currentCount; i < that.length; i++)
+                {
+                    that[i].setMap(null);
+                }
+                //Determine if the infoWindow is opened by a planned marker
+                if (infoWindow["owner"] == that && that.activeIndex >= that.currentCount)
+                {
+                    infoWindow.setAnchor(null);
+                    infoWindow["owner"] = null;
+                    that.activeIndex = -1;
+                }
+            }
+        }),
+        $currentCheckbox = $(options["currentCheckbox"])
+        .change(function ()
+        {
+            if ($(this).prop("checked"))
+            {
+                for (var i = 0; i < that.currentCount; i++)
+                {
+                    that[i].setMap(map);
+                }
+            } else
+            {
+                for (var i = 0; i < that.currentCount; i++)
+                {
+                    that[i].setMap(null);
+                }
+                if (infoWindow["owner"] == that && that.activeIndex > -1 && that.activeIndex < that.currentCount)
+                {
+                    infoWindow.setAnchor(null);
+                    infoWindow["owner"] = null;
+                    that.activeIndex = -1;
+                }
+            }
+        }),
+        infoWindow = map["infoWindow"];
+
+    this.activeIndex = -1;
+    this.plannedCount = 0;
+    this.currentCount = 0;
+    this.length = 0;
+
+    function capitalizeFirstLetter(string)
+    {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    function getinfoWindowHtml(smarker)
+    {
+        var newRowColor = "white";
+
+        function addRow(colname, data)
+        {
+            if (data != null && data.length > 0)
+            {
+                var rowHTML = "<div style=\"width:300px\" class=\"clearfix\"><h5 style=\"vertical-align:top;margin:0;display:inline;float:left;color:black !important\">" + colname + "&nbsp;</h5><ul class=\"clearfix\" style=\"max-width:200px;vertical-align:top;margin:-1px 0 0 0;padding:0;display:inline;float:left;\"><li style=\"list-style-image:none !important;margin:0 !important;font-size:0;color:white\"><span style=\"line-height:18px;color:black;font-size:13px;\">" + data + "</span></li></ul></div>";
+                newRowColor = newRowColor == "white" ? "whitesmoke" : "white";
+                table += rowHTML;
+            }
+        }
+
+
+        var isCurrent = (smarker["EventCategory"] == "Current Event");
+        var startDate = smarker.EventStartDate;
+        var endDate = smarker.EventEndDate;
+        var days = smarker.RecurrenceDescription.replace(/\n/g, "<br/>");
+        var location = smarker.LocationDescription;
+        var header = "<h2 style=\"color:black !important;vertical-align:top;text-align:center;margin:0 0 10px 0\">&nbsp;";
+        //header += smarker.EventType;
+        header += isCurrent ? "Incident" : "Road&nbsp;Work";
+        header += "&nbsp;</h2>";
+        var eventType = (smarker.EventSubType.length > 0 && smarker.EventSubType != "Other") ? (smarker.EventSubType) : "";
+
+        var table = "<div style=\"\" class=\"clearfix\">";
+        table += header;
+        addRow("Type:", eventType);
+        if (isCurrent)
+        {
+            addRow("Date&nbsp;Reported:", startDate);
+        }
+        else
+        {
+            addRow("Start&nbsp;Date:", startDate);
+            addRow("End&nbsp;Date:", endDate);
+            addRow("Days/Times:", days);
+        }
+        addRow("Location:", location);
+        return table + "</div>";
+    }
+
+
+    var ers_markers = options["data"];
+
+    //Loops through ersJSON and adds the markers to the map
+    that.length = ers_markers.length;
+    var plannedStartValue = $plannedCheckbox.prop("checked") ? map : null;
+    var currentStartValue = $currentCheckbox.prop("checked") ? map : null;
+
+    $.each(ers_markers, function (i, value)
+    {
+        var
+            isCurrent = value["EventCategory"] == "Current Event",
+            image = options["imagesFolder"] + (isCurrent ? options["currentImage"] : options["plannedImage"]),
+            myLatLng = new google.maps.LatLng(value.PrimaryLatitude, value.PrimaryLongitude),
+            marker = new google.maps.Marker({
+                position: myLatLng,
+                title: value.RoadwayName + ((value.EventSubType.length > 0 && value.EventSubType != "Other") ? (" (" + value.EventSubType) + ")" : ""),
+                clickable: true,
+                map: isCurrent?currentStartValue:plannedStartValue,
+                icon: image,
+                height: 24,//50,
+                width: 24,//22,
+                index: i,
+                anchor: new google.maps.Point(12, 12)
+            });
+
+        //Allow overriding of marker click by specifying a click function in the constructor options
+        if (typeof options["click"] !== "undefined")
+        {
+            google.maps.event.addListener(marker, 'click', options["click"]);
+        } else //Default marker click behavior
+        {
+            google.maps.event.addListener(marker, 'click', function ()
+            {
+                var content = getinfoWindowHtml(value);
+                infoWindow.setContent(content);
+                infoWindow.open(map, marker);
+                infoWindow["owner"] = that;
+                that.activeIndex = i;
+            });
+        }
+
+        //Add optional mouse events if specified in constructor options
+        if (typeof options["mouseover"] !== "undefined")
+        {
+            google.maps.event.addListener(marker, 'mouseover', options["mouseover"]);
+        }
+        if (typeof options["mouseout"] !== "undefined")
+        {
+            google.maps.event.addListener(marker, 'mouseout', options["mouseout"]);
+        }
+
+        that[i] = marker;
+        if (isCurrent) { that.currentCount++; }
+    });
+
+    
+    that.plannedCount = that.length - that.currentCount;
+    //Remove references to large JSON objects (so they can be collected as garbage)
+    options["data"] = null;
+
+};
+
+mdot["maps"]["plugins"]["ers"]["applyToMap"] = mdot["maps"]["plugins"]["ers"]["applyToMap"] || function (map, options)
+{
+    "use strict";
+    map["ersIncidents"] = new mdot["maps"]["plugins"]["ers"]["Incidents"](map, options);
+};
