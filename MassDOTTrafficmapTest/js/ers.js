ed52e2754dffdb93fb6ec010e36691c68321524d
GovindarajanMa <31079345+GovindarajanMa@users.noreply.github.com>
2021-04-11 18:11:00 -0400

    Update ers.js

--- MassDOTTrafficmapTest/js/ers.js
+++ MassDOTTrafficmapTest/js/ers.js
@@ -103,9 +103,10 @@ mdot["maps"]["plugins"]["ers"]["Incidents"] = mdot["maps"]["plugins"]["ers"]["In
         
         if (isCurrent)
         {
-            alert(((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear());
+            //alert(((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear());
             //alert("@@@@@@ iscurrent  " + startDate);
-            addRow("Date&nbsp;Reported:", startDate);
+            var startDateFormat = ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear();
+            addRow("Date&nbsp;Reported:", startDateFormat);
         }
         else
         {
