48247bc023aff90d6a8ebb58daec4d648045cd8d
GovindarajanMa <31079345+GovindarajanMa@users.noreply.github.com>
2021-04-13 10:57:02 -0400

    Update ersTest.js

--- MassDOTTrafficmapTest/js/ersTest.js
+++ MassDOTTrafficmapTest/js/ersTest.js
@@ -96,8 +96,8 @@ mdot["maps"]["plugins"]["ers"]["Incidents"] = mdot["maps"]["plugins"]["ers"]["In
         var startDate = smarker.EventCreatedDate;
         
         //alert("smarkerEnddt  " + smarker.EventEndDate);
-        alert(smarker.RecurrenceDescription);
-        var days = smarker.RecurrenceDescription.replace(/\n/g, "<br/>");
+        alert(smarker.RecurrenceDescription.replace(" T","<br/>"));
+        var days = smarker.RecurrenceDescription.replace(" T","<br/>");
         var location = smarker.LocationDescription;
         var header = "<h2 style=\"color:black !important;vertical-align:top;text-align:center;margin:0 0 10px 0\">&nbsp;";
         //header += smarker.EventType;
